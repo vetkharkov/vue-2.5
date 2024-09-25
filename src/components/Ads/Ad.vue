0ff04ff70aa75d7a586a52a03199d01146a27dcb
vetkharkov <bvpkharkov@gmail.com>
2024-09-25 19:47:33 +0300

    add View Card

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,21 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <h1>Ad</h1>
+        <v-card>
+          <v-card-media
+            src="https://picsum.photos/1100/500"
+            height="300px"
+          ></v-card-media>
+          <v-card-text>
+            <h1 class="text--primary">lorem</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, ipsa?</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="warning" flat>Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
