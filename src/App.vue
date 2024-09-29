906b287a2fdd2ad365b4668adb8438774dc45742
vetkharkov <bvpkharkov@gmail.com>
2024-09-29 08:39:57 +0300

    add Logout and Login

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <v-app>
-    <v-navigation-drawer app temporary v-model="drawer">
+    <v-navigation-drawer
+      app
+      temporary
+      v-model="drawer"
+    >
       <v-list>
-        <v-list-tile v-for="link of links" :key="link.title" :to="link.url">
+        <v-list-tile
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url"
+        >
           <v-list-tile-action>
-            <v-icon>{{ link.icon }}</v-icon>
+            <v-icon>{{link.icon}}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile
+          v-if="isUserLoggedIn"
+          @click="onLogout"
+        >
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="'Logout'"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
 
     <v-toolbar app dark color="primary">
-      <v-toolbar-side-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-side-icon
+        @click="drawer = !drawer"
+        class="hidden-md-and-up"
+      ></v-toolbar-side-icon>
       <v-toolbar-title>
         <router-link to="/" tag="span" class="pointer">Ad application</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn v-for="link in links" :key="link.title" :to="link.url" flat>
-          <v-icon left>{{ link.icon }}</v-icon>
-          {{ link.title }}
+        <v-btn
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url"
+          flat
+        >
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}
+        </v-btn>
+        <v-btn
+          @click="onLogout"
+          flat
+          v-if="isUserLoggedIn"
+        >
+          <v-icon left>exit_to_app</v-icon>
+          Logout
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -44,24 +79,38 @@
 export default {
   data() {
     return {
-      drawer: false,
-      links: [
-        { title: 'Login', icon: 'lock', url: '/login' },
-        { title: 'Registration', icon: 'face', url: '/registration' },
-        { title: 'Orders', icon: 'bookmark_border', url: '/orders' },
-        { title: 'New ad', icon: 'note_add', url: '/new' },
-        { title: 'My ads', icon: 'list', url: '/list' }
-      ]
+      drawer: false
     }
   },
   computed: {
     error() {
       return this.$store.getters.error
+    },
+    isUserLoggedIn() {
+      return this.$store.getters.isUserLoggedIn
+    },
+    links() {
+      if (this.isUserLoggedIn) {
+        return [
+          { title: 'Orders', icon: 'bookmark_border', url: '/orders' },
+          { title: 'New ad', icon: 'note_add', url: '/new' },
+          { title: 'My ads', icon: 'list', url: '/list' }
+        ]
+      }
+
+      return [
+        { title: 'Login', icon: 'lock', url: '/login' },
+        { title: 'Registration', icon: 'face', url: '/registration' }
+      ]
     }
   },
   methods: {
     closeError() {
       this.$store.dispatch('clearError')
+    },
+    onLogout() {
+      this.$store.dispatch('logoutUser')
+      this.$router.push('/')
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
-import { initializeApp } from 'firebase/app'
+// import { initializeApp } from 'firebase/app'
+import * as firebase from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -18,13 +19,19 @@ new Vue({
   components: { App },
   template: '<App/>',
   created() {
-    initializeApp({
+    firebase.initializeApp({
       apiKey: 'AIzaSyDmAQU6EaqDNK-bVOeCwmxDoSTBsMzPW_8',
       authDomain: 'it-project-1c93c.firebaseapp.com',
       projectId: 'it-project-1c93c',
       storageBucket: 'it-project-1c93c.appspot.com',
       messagingSenderId: '14050287136',
       appId: '1:14050287136:web:6adc09b1f5028c2f062645'
+    })
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$store.dispatch('autoLoginUser', user)
+      }
     })
   }
 })
